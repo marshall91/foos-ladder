b08f3999c6a4de4f79bd79b67d0511b968c998d1
Mackenzie Marshall <mack.j.marshall@gmail.com>
2015-02-09 16:03:41 -0800

    Minor code style fixes?

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,11 +7,11 @@ import scala.slick.lifted.ProvenShape
 
 case class Member(id: Int, name: String, email: String, avatar: String, position: Int)
 
-class Members(tag: Tag) extends Table[(Int, String, String, String, Int)](tag,"member") {
-  def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+class Members(tag: Tag) extends Table[(Option[Int], String, String, Option[String], Int)](tag,"member") {
+  def id = column[Option[Int]]("id", O.PrimaryKey, O.AutoInc)
   def name = column[String]("name", O.NotNull)
   def email = column[String]("email", O.NotNull)
-  def avatar = column[String]("avatar")
+  def avatar = column[Option[String]]("avatar")
   def position = column[Int]("position", O.NotNull)
   def * = (id, name, email, avatar, position)
 }
@@ -25,14 +25,13 @@ object Application extends Controller {
   def insertMember = Action {
     val members = TableQuery[Members]
     val db = Database.forURL(
-      "jdbc:mysql://us-cdbr-iron-east-01.cleardb.net/heroku_3527ddcd678c81b?reconnect=true",
-      "bab3372bccec58",
-      "f200ff41",
-      null,
-      "com.mysql.jdbc.Driver"
+      url = "jdbc:mysql://us-cdbr-iron-east-01.cleardb.net/heroku_3527ddcd678c81b?reconnect=true",
+      user = "bab3372bccec58",
+      password = "f200ff41",
+      driver = "com.mysql.jdbc.Driver"
     )
     db.withSession { implicit session =>
-      members +=(1, "Mackenzie Marshall", "mackenzie.marshall@hootsuite.com", null, 1)
+      members +=(None, "Bob Testing 3", "mackenzie.marshall@hootsuite.com", None, 1)
     }
 
     Ok("We're good")
